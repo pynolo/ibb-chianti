9ad154a565566b5de6922450307107f8a2f65f7e
Pynolo <pynolo@tarine.net>
2016-12-25 23:05:05 +0100

    Cleaning/stripping process is progressing

--- src/main/java/net/tarine/ibbchianti/client/frame/IWizardPanel.java
+++
@@ -1,8 +0,0 @@
-package net.tarine.ibbchianti.client.frame;
-
-public interface IWizardPanel {
-
-	public void goForward();
-	public void goBackward();
-	
-}
--- src/main/java/net/tarine/ibbchianti/client/frame/JoinStart2Frame.java
+++ src/main/java/net/tarine/ibbchianti/client/frame/JoinStart2Frame.java
@@ -1,19 +1,23 @@
 package net.tarine.ibbchianti.client.frame;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.InlineHTML;
-import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DateBox;
 
-import net.tarine.ibbchianti.client.ClientConstants;
 import net.tarine.ibbchianti.client.LocaleConstants;
 import net.tarine.ibbchianti.client.UiSingleton;
 import net.tarine.ibbchianti.client.UriBuilder;
@@ -25,24 +29,27 @@ import net.tarine.ibbchianti.client.service.DataServiceAsync;
 import net.tarine.ibbchianti.client.widgets.ExtendedTextBox;
 import net.tarine.ibbchianti.client.widgets.WizardButtons;
 import net.tarine.ibbchianti.shared.AppConstants;
-import net.tarine.ibbchianti.shared.PropertyBean;
 import net.tarine.ibbchianti.shared.StringValidator;
 import net.tarine.ibbchianti.shared.ValidationException;
 import net.tarine.ibbchianti.shared.entity.Participant;
 
-public class JoinStart2Frame extends FramePanel implements IWizardPanel {
+public class JoinStart2Frame extends FramePanel {
 	
 	private final DataServiceAsync dataService = GWT.create(DataService.class);
 	private LocaleConstants constants = GWT.create(LocaleConstants.class);
-	private PropertyBean pb = null;
+	
+	private DateTimeFormat DTF = DateTimeFormat.getFormat("dd/MM/yyyy");
 	
 	private UriBuilder params = null;
 	private VerticalPanel cp = null; // Content panel
 	
-	private RadioButton hutRadio;
-	private RadioButton tentRadio;
 	private ExtendedTextBox emailText;
-	private InlineHTML discountLabel;
+	private TextBox firstNameText;
+	private TextBox lastNameText;
+	private DateBox birthDate;
+	private TextBox birthCityText;
+	private CheckBox ibbCheck;
+	private CheckBox burnerCheck;
 	
 	public JoinStart2Frame(UriBuilder params) {
 		super();
@@ -51,124 +58,84 @@ public class JoinStart2Frame extends FramePanel implements IWizardPanel {
 		} else {
 			this.params = new UriBuilder();
 		}
-		String itemNumber = this.params.getValue(AppConstants.PARAMS_ITEM_NUMBER);
-		if (itemNumber == null) itemNumber = "";
 		cp = new VerticalPanel();
 		this.add(cp);
-		pb = WizardSingleton.get().getPropertyBean();
-		loadAsyncData(itemNumber);
+		loadAsyncData();
 	}
 	
-	private void draw() {		
-		if (WizardSingleton.get().getWizardType().equals(AppConstants.WIZARD_REGISTER))
-				forwardIfJoinNotPossible();
+	private void draw() {
 		Participant participant = WizardSingleton.get().getParticipantBean();
-		
-		//TITLE
-		setTitle(constants.joinBaseTitle());
-		
-		VerticalPanel formPanel = new VerticalPanel();
-		cp.add(formPanel);
-		formPanel.add(new HTML("<p>"+constants.joinBaseWelcome()+"</p>"));
-		
+		cp.clear();
+
 		//EMAIL
-		
-		formPanel.add(new HTML("<p>"+constants.joinBaseEmail()+"</p>"));
+		cp.add(new HTML("<p>"+constants.joinBaseEmail()+"</p>"));
 		HorizontalPanel emailPanel = new HorizontalPanel();
 		emailText = new ExtendedTextBox();
 		emailText.setValue(participant.getEmail());
 		emailPanel.add(emailText);
 		emailPanel.add(new InlineHTML("&nbsp;&nbsp;"));
-		discountLabel = new InlineHTML();
-		emailPanel.add(discountLabel);
-		formPanel.add(emailPanel);
-		formPanel.add(new HTML("<p><i>"+constants.joinBaseEmailWarning()+"</i></p>"));
+		cp.add(emailPanel);
+		cp.add(new HTML("<p><i>"+constants.joinBaseEmailWarning()+"</i></p>"));
+		
+		//TITLE
+		setTitle(constants.joinLegalTitle());
+		
+		cp.add(new HTML("<p>"+constants.joinLegalItalianLaw()+"</p>"));
+		
+		HorizontalPanel namePanel = new HorizontalPanel();
+		cp.add(namePanel);
 
-		emailText.addKeyUpHandler(new KeyUpHandler() {
-			@Override
-			public void onKeyUp(KeyUpEvent event) {
-				if (emailText.getValue() != null) {
-					onEmailChange(emailText.getValue());
-				}
-			}
-		});
-		emailText.addValueChangeHandler(new ValueChangeHandler<String>() {
-			@Override
-	        public void onValueChange(ValueChangeEvent<String> event) {
-	        	if (emailText.getValue() != null) {
-	        		onEmailChange(emailText.getValue());
-				}
-	        }
-	    });
-		formPanel.add(new HTML("<p>&nbsp;</p>"));
+		FlowPanel firstNamePanel = new FlowPanel();
+		firstNamePanel.add(new HTML(constants.joinLegalFirstName()));
+		firstNameText = new TextBox();
+		firstNameText.setMaxLength(64);
+		firstNameText.setValue(participant.getFirstName());
+		firstNamePanel.add(firstNameText);
+		namePanel.add(firstNamePanel);
 		
-		//ACCOMMODATION
-		formPanel.add(new HTML("<p>"+constants.joinBaseAccommodation()+"</p>"));
+		namePanel.add(new InlineHTML("&nbsp;"));
 		
-		FlexTable accmTable = new FlexTable();
-		formPanel.add(accmTable);
+		FlowPanel lastNamePanel = new FlowPanel();
+		lastNamePanel.add(new HTML(constants.joinLegalLastName()));
+		lastNameText = new TextBox();
+		lastNameText.setMaxLength(64);
+		lastNameText.setValue(participant.getLastName());
+		lastNamePanel.add(lastNameText);
+		namePanel.add(lastNamePanel);
 		
-		//hut radio
-		hutRadio = new RadioButton("accommodation");
-		accmTable.setWidget(0, 0, hutRadio);
-		//hut img
-		accmTable.setWidget(0, 1, new HTML("<p align='center'>&nbsp;<img src='img/hut.png' /></p>"));
-		accmTable.getFlexCellFormatter().setRowSpan(0, 1, 2);
-		if (pb.getAvailableHut() > 0) {
-			//hut descr
-			String hutDescr = "";
-			if (pb.getAvailableHut() <= 4) {
-				hutDescr += "<b>"+pb.getAvailableHut()+"</b> "+constants.joinBaseToSoldOut()+"<br />";
-			}
-			hutDescr += constants.joinBaseBedFeatures();
-			accmTable.setWidget(1, 0, new HTML(hutDescr+"<br/>&nbsp;"));
-		} else {
-			hutRadio.setEnabled(false);
-			accmTable.setWidget(1, 0, new HTML(constants.hut()+" SOLD OUT <br/>&nbsp;"));
-		}
-
-		//tent radio
-		tentRadio = new RadioButton("accommodation");
-		accmTable.setWidget(2, 0, tentRadio);
-		//tent img
-		accmTable.setWidget(2, 1, new HTML("<p align='center'>&nbsp;<img src='img/tent.png' /></p>"));
-		accmTable.getFlexCellFormatter().setRowSpan(2, 1, 2);
-		if (pb.getAvailableTent() > 0) {
-			//tent descr
-			String tentDescr = "";
-			if (pb.getAvailableTent() <= 4) {
-				tentDescr += "<b>"+pb.getAvailableTent()+"</b> "+constants.joinBaseToSoldOut()+"<br />";
-			}
-			tentDescr += constants.joinBaseTentFeatures();
-			accmTable.setWidget(3, 0, new HTML(tentDescr+"<br/>&nbsp;"));
-		} else {
-			tentRadio.setEnabled(false);
-			accmTable.setWidget(3, 0, new HTML(constants.tent()+" SOLD OUT <br/>&nbsp;"));
-		}
+		cp.add(new HTML("<p>&nbsp;</p>"));
 		
-		//Switch
-		if (tentRadio.isEnabled()) tentRadio.setValue(true);
-		if (hutRadio.isEnabled()) hutRadio.setValue(true);
-		if (participant.getAccommodationType().equals(AppConstants.ACCOMMODATION_TENT)) {
-			if (tentRadio.isEnabled()) tentRadio.setValue(true);
-		} else {
-			if (hutRadio.isEnabled()) hutRadio.setValue(true);
-		}
+		HorizontalPanel birthPanel = new HorizontalPanel();
+		cp.add(birthPanel);
+		
+		FlowPanel birthCityPanel = new FlowPanel();
+		birthCityPanel.add(new HTML("<p>"+constants.joinLegalBirthCity()+"</p>"));
+		birthCityText = new TextBox();
+		birthCityText.setMaxLength(128);
+		birthCityText.setValue(participant.getBirthCity());
+		birthCityPanel.add(birthCityText);
+		birthPanel.add(birthCityPanel);
+		
+		birthPanel.add(new InlineHTML("&nbsp;"));
+		
+		FlowPanel birthDatePanel = new FlowPanel();
+		birthDatePanel.add(new HTML("<p>"+constants.joinLegalBirthDate()+"</p>"));
+		birthDate = new DateBox();
+		DateBox.Format BOX_FORMAT_TIMESTAMP = new DateBox.DefaultFormat(DTF);
+		birthDate.setFormat(BOX_FORMAT_TIMESTAMP);
+		birthDate.setValue(participant.getBirthDt());
+		birthDatePanel.add(birthDate);
+		birthPanel.add(birthDatePanel);
+		
+		cp.add(new HTML("<p>&nbsp;</p>"));
 		
 		//Wizard panel
-		WizardButtons wb = new WizardButtons(this, false, true);
+		WizardButtons wb = new WizardButtons(this, true, true);
 		cp.add(wb);
-		
-		onEmailChange(emailText.getValue());
 	}
 	
-	@Override
-	public void goBackward() {
-		/* disabled */
-	}
 	
-	@Override
-	public void goForward() {
+	private boolean storeInBean() {
 		//Verification
 		String errorMessage = "";
 		String email = emailText.getValue();
@@ -178,45 +145,49 @@ public class JoinStart2Frame extends FramePanel implements IWizardPanel {
 			if (errorMessage.length() > 0) errorMessage += "<br />";
 			errorMessage += e.getMessage();
 		}
+		String firstName = firstNameText.getValue();
+		try {
+			StringValidator.validateName(firstName);
+		} catch (ValidationException e) {
+			errorMessage += e.getMessage();
+		}
+		String lastName = lastNameText.getValue();
+		try {
+			StringValidator.validateName(lastName);
+		} catch (ValidationException e) {
+			if (errorMessage.length() > 0) errorMessage += "<br />";
+			errorMessage += e.getMessage();
+		}
+		String birthCity = birthCityText.getValue();
+		if (birthCity.length() < 3) {
+			if (errorMessage.length() > 0) errorMessage += "<br />";
+			errorMessage += constants.joinLegalErrorCity();
+		}
+		Date birthDt = birthDate.getValue();
+		if (birthDt == null) {
+			if (errorMessage.length() > 0) errorMessage += "<br />";
+			errorMessage += constants.joinLegalErrorDate();
+		}
 		
-		if (errorMessage.length() > 0) {
+		boolean isError = (errorMessage.length() > 0);
+		if (isError) {
 			UiSingleton.get().addWarning(errorMessage);
 		} else {
 			//Store in bean
 			Participant participant = WizardSingleton.get().getParticipantBean();
-			if (tentRadio.getValue()) {
-				participant.setAccommodationType(AppConstants.ACCOMMODATION_TENT);
-			} else {
-				participant.setAccommodationType(AppConstants.ACCOMMODATION_HUT);
-			}
 			participant.setEmail(email);
-			//Forward
-			UriBuilder param = new UriBuilder();
-			param.add(AppConstants.PARAMS_ITEM_NUMBER, participant.getItemNumber());
-			param.triggerUri(UriDispatcher.STEP_JOIN_VOLUNTEER);
+			participant.setFirstName(firstName);
+			participant.setLastName(lastName);
+			participant.setBirthCity(birthCity);
+			participant.setBirthDt(birthDt);
 		}
+		return isError;
 	}
 	
-	private void hideDiscount() {
-		discountLabel.setHTML("<i class='fa fa-comment-o'></i> "+constants.discountNo());
-		hutRadio.setHTML(" <b>"+constants.hut()+"</b> - &euro;"+
-				ClientConstants.FORMAT_CURRENCY.format(pb.getHutPrice())+"&nbsp;&nbsp;");
-		tentRadio.setHTML(" <b>"+constants.tent()+"</b> - &euro;"+
-				ClientConstants.FORMAT_CURRENCY.format(pb.getTentPrice())+"&nbsp;&nbsp;");
-	}
-	
-	private void showDiscount() {
-		discountLabel.setHTML("<i class='fa fa-check-circle'></i> "+constants.discountYes());
-		hutRadio.setHTML(" <b>"+constants.hut()+"</b> - &euro;"+
-				ClientConstants.FORMAT_CURRENCY.format(pb.getHutPriceLow())+
-				" ("+constants.discount()+")&nbsp;&nbsp;");
-		tentRadio.setHTML(" <b>"+constants.tent()+"</b> - &euro;"+
-				ClientConstants.FORMAT_CURRENCY.format(pb.getTentPriceLow())+
-				" ("+constants.discount()+")&nbsp;&nbsp;");
-	}
 	
 	
 	//Async methods
+	
 	
 	
 	private void loadAsyncData(String itemNumber) {
@@ -236,8 +207,8 @@ public class JoinStart2Frame extends FramePanel implements IWizardPanel {
 		
 		if (itemNumber.length() == 0) {
 			//No itemNumber passed => brand new participant
-			dataService.createTransientParticipant(callback);
 			WaitSingleton.get().start();
+			dataService.createTransientParticipant(callback);
 		} else {
 			//itemNumberKey passed => check participant in WizardSingleton and load it from DB if empty
 			Participant prt = WizardSingleton.get().getParticipantBean();
@@ -248,28 +219,6 @@ public class JoinStart2Frame extends FramePanel implements IWizardPanel {
 				draw();
 			}
 		}
-	}
-	
-	
-	public void onEmailChange(String email) {
-		AsyncCallback<Boolean> callback = new AsyncCallback<Boolean>() {
-			@Override
-			public void onFailure(Throwable caught) {
-				UiSingleton.get().addError(caught);
-				hideDiscount();
-			}
-			@Override
-			public void onSuccess(Boolean result) {
-				if (result == null) result = false;
-				if (result) {
-					showDiscount();
-				} else {
-					hideDiscount();
-				}
-			}
-		};
-		hideDiscount();
-		dataService.canHaveDiscount(email, callback);
 	}
 	
 }
